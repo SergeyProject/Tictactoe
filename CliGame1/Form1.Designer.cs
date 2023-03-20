86f03f87e26daee1c5adab7f596af11aee01b03b
SergeyProject <86799609+SergeyProject@users.noreply.github.com>
2023-03-20 20:42:33 +0300

    Проект Tictactoe - web api игры в крестики нолики 3x3 для двух игроков. Проект CliGame1 - клиент игры WinForm

---
+++ CliGame1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace CliGame1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button10 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(73, 230);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 4;
+            this.button10.Text = "New Game";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 203);
+            this.tableLayoutPanel1.TabIndex = 3;
+            this.tableLayoutPanel1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(225, 263);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CliGame1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CliGame1
+{
+    public partial class Form1 : Form
+    {
+        string hostUrl;
+        public Form1()
+        {
+            InitializeComponent();
+            hostUrl = ConfigurationManager.AppSettings.Get("url");
+            LoadBut();
+        }
+        Button[] buttons = new Button[9];
+        private void LoadBut()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                buttons[i] = new Button();
+                buttons[i].Tag= i;
+                buttons[i].Font=new Font("Tahoma", 22);
+                tableLayoutPanel1.Controls.Add(buttons[i]);
+                buttons[i].Dock = DockStyle.Fill;
+                buttons[i].Click += new EventHandler(button_Click);
+            }
+        }
+        private async void button_Click(object sender, EventArgs e)
+        {              
+            Button b = (Button)sender;
+            string url = $"{hostUrl}/Tictactoe/api/GetStep?idx={b.Tag}";
+            var client = new HttpClient();
+            await client.GetStringAsync(url);
+            string val = await client.GetStringAsync($"{hostUrl}/Tictactoe/api/GetValue");
+            b.Text = val.Replace("\"", "");
+            var content = await client.GetStringAsync($"{hostUrl}/Tictactoe/api/TestWin");
+            if (content.Contains("Winner"))
+            {
+                timer1.Stop();
+                MessageBox.Show(content.Replace("\"", ""));
+            }           
+        }
+
+        
+        private async Task<string> SendReqAsync(string url)
+        {
+            var client = new HttpClient();
+            var content = await client.GetStringAsync(url);
+
+            return content;  //.Replace("\"", "");
+           
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            NewGame();
+            tableLayoutPanel1.Visible = true;
+            for (int i = 0; i < 9; i++)
+            {
+                buttons[i].Text = "";
+            }
+            timer1.Start();
+        }
+
+        private async void NewGame()
+        {
+            var client = new HttpClient();
+            var content = await client.GetStringAsync($"{hostUrl}/Tictactoe/api/NewGame");
+
+            //MessageBox.Show(content);
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                var client = new HttpClient();
+                var _idx = await client.GetStringAsync($"{hostUrl}/Tictactoe/api/GetIdx");
+                int idx = int.Parse(_idx);
+                string val = await client.GetStringAsync($"{hostUrl}/Tictactoe/api/GetValue");
+                if (val != "null")
+                    buttons[idx].Text = val.Replace("\"", "");
+                var content = await client.GetStringAsync($"{hostUrl}/Tictactoe/api/TestWin");
+                if (content.Contains("Winner"))
+                {
+                    timer1.Stop();
+                    MessageBox.Show(content.Replace("\"", ""));
+                }
+            }
+            catch { }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var _idx = await client.GetStringAsync($"{hostUrl}/Tictactoe/api/GetMap");
+
+           var map = JsonConvert.DeserializeObject<Mapmodel>(_idx);
+            int[] idx = map.map;
+            foreach(var i in idx)
+            {
+                MessageBox.Show($"{i}");
+            }
+        }       
+    }
+
+    class Mapmodel
+    {
+        public int[] map { get; set; }
+    }
+}
